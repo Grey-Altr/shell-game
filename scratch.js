28a2671375fe5a566dcdb148797717cb6da42581
Grey Altr <grey.sifr@gmail.com>
2022-07-31 16:22:20 -0700

    working on incorporating an array

---
+++ scratch.js
@@ -0,0 +1,13 @@
+const cups = [imgOne, imgTwo, imgThree]
+
+function getRandomItem(arr) {
+    const randomCupNumber = Math.floor(Math.random() * 3);
+    return arr[randomCupNumber]
+}
+
+function cupGameLogic {
+    resetImages();
+    total++;
+    const winningCup = getRandomItem(cups)
+    
+}
